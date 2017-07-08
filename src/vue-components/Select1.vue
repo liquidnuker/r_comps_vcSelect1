37df381f355036189029e9653134246f89206d7c
administrator <liquidnuker@users.noreply.github.com>
2017-07-08 11:00:01 -0700

    added dynamic items

--- src/vue-components/Select1.vue
+++ src/vue-components/Select1.vue
@@ -1,24 +1,40 @@
 <template>
   <div class="vcselect1">
     <div class="custom-select1_01">
-    <select id="cat2">
-      <option>item1</option>
-      <option>item2</option>
-      <option>item3</option>
-      <option>item4</option>
-      <option>Lorem ipsum dolor sit amet sed do eiusmod</option>
+    <select id="cat2" v-model="selected">
+      <option v-for="i in options" v-bind:value="i"
+      v-on:click="switchCategory(i.itemName)">
+        {{ i.itemName }}
+      </option>
     </select>
-  </div><input type="button" value="GO!">
+  </div><input type="button" value="GO!" v-on:click="switchCategory(selected.itemName)">
 </div>
 </template>
 <script>
   export default {
     data () {
       return {    
+        selected: {itemName: "item1"},
+        options: [
+          {
+            itemName: "item1"
+          },
+          {
+            itemName: "item2"
+          },
+          {
+            itemName: "item3"
+          }
+        ]
       }
     },
     mounted: function () {
       console.log("Select1.vue mounted");
+    },
+    methods: {
+      switchCategory: function(item) {
+        console.log(item);
+      }
     }
   }
 </script>
